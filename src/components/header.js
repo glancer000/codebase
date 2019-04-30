96a828faa453b562e03ea84c2177e1e03bed65f2
Greg <contactgregtucker@gmail.com>
2019-04-30 17:06:18 -0300

    added gatsby sass

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import { Link } from 'gatsby'
+import headerStyles from './header.module.scss'
 
 const Header = () => {
     return(
         <header>
         <nav>
             <ui>
-                <li><Link to='/'>Home</Link></li>
+                <li><Link className={headerStyles.link} to='/'>Home</Link></li>
                 <li><Link to='/blog'>Blog</Link></li>
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/contact'>Contact</Link></li>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import React from "react"
 
 import Layout from '../components/layout'
 
+import '../styles/index.scss'
+
 const IndexPage = () => {
     return(
         <Layout>
