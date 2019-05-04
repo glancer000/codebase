ad0ab965a295a6ab7303afbba08dc00c8063d08e
glancer000 <glancer000@gmail.com>
2019-05-04 14:47:31 -0300

    added logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,24 +2,26 @@ import React from 'react'
 
 import { Link} from 'gatsby'
 import navStyles from './nav.module.scss'
+import logo from '../images/codebase.png';
 
 
 const Nav = () => {
 
     return(
-        <header className={navStyles.container}> 
-            <div className={navStyles.logo}>
-                <Link className={navStyles.title} to ="/">logo</Link>
-            </div>
-            <nav className={navStyles.menu}>
+        <nav className={navStyles.container}>
+            <Link className={navStyles.logo} to ="/"><img src={logo} alt="Logo" /></Link>
+            <div className={navStyles.spacer}></div>
+            <div className={navStyles.nav}>
                 <ui className={navStyles.navList}>
                     <li><Link className={navStyles.navItem} activeClassName={navStyles.activeNavItem} to='/'>Home</Link></li>
                     <li><Link className={navStyles.navItem} activeClassName={navStyles.activeNavItem} to='/services'>Services</Link></li>
                     <li><Link className={navStyles.navItem} activeClassName={navStyles.activeNavItem} to='/blog'>Resources</Link></li>
                     <li><Link className={navStyles.navItem} activeClassName={navStyles.activeNavItem} to='/about'>About</Link></li>
                 </ui>
-            </nav>
-        </header>
+            </div>
+            <div>Start your Project</div>
+        </nav>
+            
     )
 }
 
